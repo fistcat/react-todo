6f13dc0cb6d77cf2d14165760672a904762ecc9f
Chenxing <chenxing1991@yahoo.com>
2016-12-31 16:14:50 -0500

    finish the search function

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -16,11 +16,35 @@ module.exports ={
 
     }
     return $.isArray(todos)? todos : [];
+  },
+  filterTodos : function(todos, showCompleted, searchText){
+    var filteredTodos = todos;
 
-    // if($,isArray(todos)){
-    //   return todos;
-    // }else{
-    //   return [];
-    // }
+    //Filter by showCOmpleted
+    filteredTodos = filteredTodos.filter((todo)=>{
+      return !todo.completed || showCompleted;
+    });
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo)=>{
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) >-1;
+
+    });
+
+    //return -1 a come before b
+    //return 1  b come before a
+    //return 0 nothing happen
+    //Sort todos with non-completed first
+    //used the build in sort function
+    filteredTodos.sort((a,b) =>{
+      if(!a.completed && b.completed){
+        return -1
+      }else if(a.completed && !b.completed){
+        return 1;
+      }else{
+        return 0;
+      }
+    });
+    return filteredTodos;
   }
 };
