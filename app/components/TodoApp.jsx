96d57eecd59e2866055fe91c05229ec82ab6ec02
Chenxing <chenxing1991@yahoo.com>
2017-01-06 14:00:20 -0500

    add github link and update readme

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -55,10 +55,10 @@ var TodoApp = React.createClass({
 
     return(
       <div>
-        <h1 className='page-title'>Todo App</h1>
-
+        <h1 className='page-title'>React Todo App</h1>
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
+            <a className = "hollow button expanded" href ="https://github.com/fistcat/react-todo">Github Repo</a>
             <div className="container">
               <TodoSearch onSearch={this.handleSearch}/>
               <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-Boilerplate React App
+React Todo App
+==================
+Running the app locally
+
+1. npm install
+2. webpack
+3. npm start
+4. go to localhost:3000
